20465710a64a7fd8fc71add85a805d223f659558
Sambhav Sharma <sambhavsharma1999@gmail.com>
2026-08-29 03:50:32 +0530

    Init - Web UI

---
+++ apps/web-healthkoins/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@healthkoins/design-system';
+
+export default function Footer() {
+  return (
+    <footer id="footer" className="footer">
+      <div className="container">
+        Footer
+
+      </div>
+    </footer>
+  );
+}
---
+++ apps/web-healthkoins/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@healthkoins/design-system';
+
+export default function Header() {
+  return (
+    <header id="header" className="header">
+      <div className="container">
+        Header
+      </div>
+    </header>
+  );
+}
